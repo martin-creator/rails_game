35ae338d45e01c57a9af0e8f679fb1ae9f029b93
martin-creator <martinlubowa@outlook.com>
2023-05-12 08:55:35 +0300

    Created first scene in the game

--- frontend/game.js
+++ frontend/game.js
@@ -1,9 +1,35 @@
 // intialize context
 // download game assests https://itch.io/
 kaboom({
-    global: true,
+    scale:3,
     width: 240,
     height: 160,
     background: [0, 0, 0],
     canvas: document.getElementById("screen"),
-})
+});
+
+loadSprite("floor", "/sprites/floor.png", { sliceX:8});
+
+scene("play", () => {
+    // add background 10x10
+    addLevel([
+        "          ",
+        "          ",
+        "          ",
+        "          ",
+        "          ",
+        "          ",
+        "          ",
+        "          ",
+        "          ",
+        "          ",
+    ], 
+    {
+        width: 16,
+        height: 16,
+        " ":() => [sprite("floor",  { frame: ~~rand(0, 8) })],
+    })
+
+});
+
+go("play")
