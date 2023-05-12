e5f2c3d74aef650e6d6aaa6b0a63ed8784b53d8e
martin-creator <martinlubowa@outlook.com>
2023-05-12 10:12:57 +0300

    Fixed height of the knight

--- frontend/game.js
+++ frontend/game.js
@@ -8,6 +8,8 @@ kaboom({
     canvas: document.getElementById("screen"),
 });
 
+const PLAYER_SPEED = 80;
+
 loadSprite("floor", "/sprites/floor.png", { sliceX: 8 });
 loadSprite("wall_left", "/sprites/wall_left.png");
 loadSprite("wall_mid", "/sprites/wall_mid.png");
@@ -16,6 +18,14 @@ loadSprite("wall_fountain", "/sprites/wall_fountain.png", {
     sliceX: 3,
     anims: {
         idle: { from: 0, to: 2, speed: 5, loop: true },
+    }
+});
+
+loadSprite("knight", "/sprites/knight.png", {
+    sliceX: 8,
+    anims: {
+        idle: { from: 0, to: 3, speed: 5, loop: true },
+        run: { from: 4, to: 7, speed: 10, loop: true },
     }
 });
 
@@ -80,7 +90,53 @@ scene("play", ({level}) => {
     // map level
     map = addLevel(matrix[level], mapConfig);
 
+    // add player
+    const player = add([
+        pos(map.getPos(2, 2)),
+        sprite("knight", { anim: "idle" }),
+        solid(), // making a player solid means it can't move through other solids
+        //area(), // area() makes the player a rectangle
+        origin("center"), // origin("center") makes the player rotate from the center
+        area({width:16, height: 16, offset: vec2(0, 8)}), // area(width:16, height: 16) makes the player a rectangle
+    ]);
+
+    onKeyDown("left", () => {
+        player.flipX(true);
+        player.move(-PLAYER_SPEED, 0);
+    });
+
+    onKeyDown("right", () => {
+        player.flipX(false);
+        player.move(PLAYER_SPEED, 0);
+    });
+
+    onKeyDown("up", () => {
+        player.move(0, -PLAYER_SPEED);
+    });
+
+    onKeyDown("down", () => {
+        player.move(0, PLAYER_SPEED);
+    });
+
+    onKeyPress(["left", "right", "up", "down"], () => {
+        player.play("run");
+    });
+
+    onKeyRelease(["left", "right", "up", "down"], () => {
+        if(
+            !keyIsDown("left") &&
+            !keyIsDown("right") &&
+            !keyIsDown("up") &&
+            !keyIsDown("down")
+        ) {
+            player.play("idle");
+        };
+    })
 
 });
 
-go("play", { level: 0})
+
+go("play", { level: 0});
+
+//debug.inspect = true;
+
